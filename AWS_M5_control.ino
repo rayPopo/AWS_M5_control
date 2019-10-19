0426389f334d4f7063e1f04c937462dd640f13f4
titov-vv <vtitov@toolbox.localdomain>
2019-10-19 15:50:51 +0300

    WiFi logic was changed

--- AWS_M5_control.ino
+++ AWS_M5_control.ino
@@ -15,7 +15,7 @@
 #include <AWS_IOT.h>
 #include <WiFi.h>
 #define ESP32
-#define CONFIG_LOG_DEFAULT_LEVEL ESP_LOG_DEBUG
+#define MAX_RETRIES 128
 //===============================================================================
 // Private data for connections are stored in another file 
 extern char WIFI_SSID[];      // name of your WiFi network
@@ -24,112 +24,170 @@ extern char HOST_ADDRESS[];   // URL of your AWS endpoint
 extern char CLIENT_ID[];      // I put thing name here
 extern char TOPIC_NAME[];     // Topic to subscribe to
 //===============================================================================
-AWS_IOT hornbill;
-int status = WL_IDLE_STATUS;
-int tick=0,msgCount=0,msgReceived = 0;
-char payload[512];
-char rcvdPayload[512];
+// Global variables
+AWS_IOT aws_client;
+int tick=0, msgCount=0, msgReceived = 0;
+char SendBuffer[1024];
+char ReceiveBuffer[1024];
 //===============================================================================
 void mySubCallBackHandler (char *topicName, int payloadLen, char *payLoad)
 {
-    strncpy(rcvdPayload, payLoad, payloadLen);
-    rcvdPayload[payloadLen] = 0;
+    strncpy(ReceiveBuffer, payLoad, payloadLen);
+    ReceiveBuffer[payloadLen] = 0;
     msgReceived = 1;
+}
+//-------------------------------------------------------------------------------
+void DoReset(char *Message)
+{
+  M5.Lcd.clear(BLUE);
+  M5.Lcd.setTextColor(TFT_RED); 
+  M5.Lcd.setCursor(0, 20);
+  M5.Lcd.print(Message);
+  M5.Lcd.setCursor(0, 40);
+  M5.Lcd.print("Rebooting in 60 sec...");
+
+  vTaskDelay(60000 / portTICK_RATE_MS);
+  ESP.restart();
+}
+//-------------------------------------------------------------------------------
+void ConnectWiFi()
+{
+  int i = 0;
+  int retry_cnt = 0;
+  
+  M5.Lcd.clear(TFT_BLUE);
+  M5.Lcd.setTextColor(TFT_WHITE);
+  M5.Lcd.setFreeFont(FM12);
+
+  strcpy(SendBuffer, "WiFi SSID: ");
+  strcat(SendBuffer, WIFI_SSID);
+  M5.Lcd.setCursor(0, 20);
+  M5.Lcd.print(SendBuffer);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  // Wait for radio connection setup
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    retry_cnt++;
+    if (retry_cnt > MAX_RETRIES)
+      DoReset("WiFi failed");
+    
+    for (i=0; i<retry_cnt; i++)
+      SendBuffer[i] = '.';
+    SendBuffer[retry_cnt] = 0;    
+    M5.Lcd.setCursor(0, 40);
+    M5.Lcd.print(SendBuffer);
+    vTaskDelay(1000 / portTICK_RATE_MS); // sleep for 1 second
+  }
+
+  M5.Lcd.clear(BLUE);
+  M5.Lcd.setTextColor(TFT_GREEN);
+  M5.Lcd.setCursor(0, 20);
+  strcpy(SendBuffer, "WiFi SSID: ");
+  strcat(SendBuffer, WIFI_SSID);
+  M5.Lcd.print(SendBuffer);
+
+  // Wait for DHCP IP assignment
+  retry_cnt = 0;
+  M5.Lcd.setTextColor(TFT_WHITE);
+  while (WiFi.localIP()[0] == 0)
+  {
+    retry_cnt++;
+    if (retry_cnt > MAX_RETRIES)
+      DoReset("DHCP failed");
+    M5.Lcd.setCursor(0, 40);
+    strcpy(SendBuffer, "IP addr: ");
+    strcat(SendBuffer, WiFi.localIP().toString().c_str());
+    M5.Lcd.print(SendBuffer); 
+    vTaskDelay(1000 / portTICK_RATE_MS); // sleep for 1 second
+  }
+  M5.Lcd.fillRect(0, 21, 320, 20, TFT_BLUE);
+  M5.Lcd.setTextColor(TFT_GREEN);
+  M5.Lcd.setCursor(0, 40);
+  strcpy(SendBuffer, "IP addr: ");
+  strcat(SendBuffer, WiFi.localIP().toString().c_str());
+  M5.Lcd.print(SendBuffer);  
 }
 //===============================================================================
 // the setup routine runs once when M5Stack starts up
 void setup()
 {
   int i = 0;
+  int retry_cnt = 0;
 
-  ESP_LOGI("test", "TEST");
   M5.begin();
-  M5.Lcd.clear(BLUE);
-  M5.Lcd.setTextColor(TFT_WHITE);
-  M5.Lcd.setFreeFont(FM12);
-
-  M5.Lcd.setCursor(0, 20);
-  M5.Lcd.print("WiFi connect");
-  while (status != WL_CONNECTED)
-  {
-    M5.Lcd.setCursor(160, 20);
-    M5.Lcd.print(i++, HEX);
-    status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    delay(5000);
-  }
-
-  M5.Lcd.clear(BLUE);
-  M5.Lcd.setCursor(0, 20);
-  M5.Lcd.print("WiFi connected");
-  vTaskDelay(3000 / portTICK_RATE_MS);
-  M5.Lcd.setCursor(0, 100);
-  M5.Lcd.print(WiFi.localIP());
-  i = hornbill.connect(HOST_ADDRESS, CLIENT_ID);
-
-  if ( i== 0)
-  {
-    M5.Lcd.setCursor(0, 40);
-    M5.Lcd.print("AWS connected");
-
-    i = hornbill.subscribe(TOPIC_NAME, mySubCallBackHandler);
-    if (i == 0)
-    {
-            M5.Lcd.setCursor(0, 60);
-            M5.Lcd.print("Subscribed");
-    }
-    else
-    {
-            M5.Lcd.setCursor(0, 60);
-            M5.Lcd.print("NOT Subscribed");
-    }
-  }
-  else
-  {
-    M5.Lcd.setCursor(0, 40);
-    M5.Lcd.print("AWS connected FAILED");
-  }
+  
+//
+//  vTaskDelay(30000 / portTICK_RATE_MS);
+//  i = aws_client.connect(HOST_ADDRESS, CLIENT_ID);
+//
+//  if ( i== 0)
+//  {
+//    M5.Lcd.setCursor(0, 40);
+//    M5.Lcd.print("AWS connected");
+//
+//    i = aws_client.subscribe(TOPIC_NAME, mySubCallBackHandler);
+//    if (i == 0)
+//    {
+//            M5.Lcd.setCursor(0, 60);
+//            M5.Lcd.print("Subscribed");
+//    }
+//    else
+//    {
+//            M5.Lcd.setCursor(0, 60);
+//            M5.Lcd.print("NOT Subscribed");
+//    }
+//  }
+//  else
+//  {
+//    M5.Lcd.setCursor(0, 40);
+//    M5.Lcd.print("AWS connected FAILED");
+//  }
 }
 //===============================================================================
 // the loop routine runs over and over again forever
 void loop() 
 {
-  int i;
-  int ypos = 20;
-    if(msgReceived == 1)
-    {
-        msgReceived = 0;
-        M5.Lcd.setCursor(160, ypos);
-        M5.Lcd.print(rcvdPayload);
-        ypos = ypos+20;
-        if (ypos > 240)
-        {
-          ypos = 20;
-        }
-    }
-    vTaskDelay(1000 / portTICK_RATE_MS); 
-    tick++;
-
-  // DEBUG
-  M5.update();
-  if (M5.BtnA.wasReleased())
-  {   
-    M5.Lcd.clear(BLUE);
-  }
-  if (M5.BtnB.wasReleased())
-  {   
-    i = hornbill.subscribe(TOPIC_NAME, mySubCallBackHandler);
-    if (i == 0)
-    {
-            M5.Lcd.setCursor(0, 60);
-            M5.Lcd.print("Subscribed");
-    }
-    else
-    {
-            M5.Lcd.setCursor(0, 60);
-            M5.Lcd.print("NOT Subscribed");
-    }
-  }
-  if (M5.BtnC.wasReleased())
-  {   
-  }
+  if (WiFi.status() != WL_CONNECTED)
+    ConnectWiFi();
+//  int i;
+//  int ypos = 20;
+//    if(msgReceived == 1)
+//    {
+//        msgReceived = 0;
+//        M5.Lcd.setCursor(160, ypos);
+//        M5.Lcd.print(ReceiveBuffer);
+//        ypos = ypos+20;
+//        if (ypos > 240)
+//        {
+//          ypos = 20;
+//        }
+//    }
+//    vTaskDelay(1000 / portTICK_RATE_MS); 
+//    tick++;
+//
+//  // DEBUG
+//  M5.update();
+//  if (M5.BtnA.wasReleased())
+//  {   
+//    M5.Lcd.clear(BLUE);
+//  }
+//  if (M5.BtnB.wasReleased())
+//  {   
+//    i = aws_client.subscribe(TOPIC_NAME, mySubCallBackHandler);
+//    if (i == 0)
+//    {
+//            M5.Lcd.setCursor(0, 60);
+//            M5.Lcd.print("Subscribed");
+//    }
+//    else
+//    {
+//            M5.Lcd.setCursor(0, 60);
+//            M5.Lcd.print("NOT Subscribed");
+//    }
+//  }
+//  if (M5.BtnC.wasReleased())
+//  {   
+//  }
 }
